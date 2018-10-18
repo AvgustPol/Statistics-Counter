9348932ccb9ba7a2a0e618418a3bd00933720059
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-19 00:45:50 +0200

    TODO : fix pmx

---
+++ StatisticsCounter/ToFileLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace StatisticsCounter
+{
+    public class ToFileLogger
+    {
+        public readonly String DEFAULT_FILE_NAME = @"D:\7 semestr\Metaheurystyki\Data\Result\newdoc.csv";
+
+        public void LogToLocalObject()
+        {
+        }
+
+        public void Log(int generationsCounter, double getBestFitness, double getAverageFitness, double getWorstFitness)
+        {
+            File.AppendAllLines(DEFAULT_FILE_NAME,
+                new[] { $"{generationsCounter},{getBestFitness},{getAverageFitness},{getWorstFitness}" });
+        }
+    }
+}
