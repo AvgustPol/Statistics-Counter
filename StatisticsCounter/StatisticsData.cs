09fec9693e8d828f0e76f2b22d5f1399220605a3
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-21 17:24:40 +0200

    Code refactoring

---
+++ StatisticsCounter/StatisticsData.cs
@@ -0,0 +1,10 @@
+﻿namespace StatisticsCounter
+{
+    public class StatisticsData
+    {
+        public int GenerationNumber { get; set; }
+        public double BestFitness { get; set; }
+        public double AverageFitness { get; set; }
+        public double WorstFitness { get; set; }
+    }
+}
--- StatisticsCounter/ToFileLogger.cs
+++ StatisticsCounter/ToFileLogger.cs
@@ -1,20 +1,43 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
 
 namespace StatisticsCounter
 {
     public class ToFileLogger
     {
-        public readonly String DEFAULT_FILE_NAME = @"D:\7 semestr\Metaheurystyki\Data\Result\newdoc.csv";
+        private string Path => _folderPath + _fileName;
+        private readonly string _folderPath = @"D:\7 semestr\Metaheurystyki\Data\Result\";
+        private readonly string _fileName;
 
-        public void LogToLocalObject()
+        private readonly List<StatisticsData> _statistics;
+
+        public ToFileLogger(string fileName)
         {
+            _fileName = fileName;
+            _statistics = new List<StatisticsData>();
         }
 
-        public void Log(int generationsCounter, double getBestFitness, double getAverageFitness, double getWorstFitness)
+        public void LogToFile()
         {
-            File.AppendAllLines(DEFAULT_FILE_NAME,
-                new[] { $"{generationsCounter},{getBestFitness},{getAverageFitness},{getWorstFitness}" });
+            File.AppendAllLines(Path,
+                new[] { $"Generation Number,Best Fitness,Average Fitness,Worst Fitness" });
+
+            foreach (var stat in _statistics)
+            {
+                File.AppendAllLines(Path,
+                    new[] { $"{stat.GenerationNumber},{stat.BestFitness},{stat.AverageFitness},{stat.WorstFitness}" });
+            }
+        }
+
+        public void LogToObject(int generationsCounter, double getBestFitness, double getAverageFitness, double getWorstFitness)
+        {
+            _statistics.Add(new StatisticsData()
+            {
+                GenerationNumber = generationsCounter,
+                AverageFitness = getAverageFitness,
+                BestFitness = getBestFitness,
+                WorstFitness = getWorstFitness
+            });
         }
     }
 }
